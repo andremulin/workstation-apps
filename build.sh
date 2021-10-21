ea11af9ba51743ab28f974a72bbcef01ab0afa37
andremulin <andremulin@ZPPE07VLNR.localdomain>
2021-10-20 23:16:08 -0300

    update requirements list

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-apt install < requirements.txt
+xargs apt install -y < requirements.txt
 
-HOST=$(netstat -nr | awk 'FNR==3{ print $0}' | awk '{ print $2}')
+HOST=$(netstat -nr | awk 'FNR==4{ print $0}' | awk '{ print $1}')
 
 sed "s/to_change/$HOST/g" hosts_template.yml > hosts.yml
 
