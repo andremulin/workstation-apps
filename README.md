6a94fb110cc8450a8f3c0dd4ae6bf2b91e8f33b9
andremulin <aesmulin@gmail.com>
2021-10-20 22:19:39 -0300

    creating ssh connection option

---
+++ README.md
@@ -0,0 +1,2 @@
+## Workstation-Apps
+[Instalar OpenSSh Server no Windows 10](https://docs.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse)
--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+apt install < requirements.txt
+
 HOST=$(netstat -nr | awk 'FNR==3{ print $0}' | awk '{ print $2}')
 
 sed "s/to_change/$HOST/g" hosts_template.yml > hosts.yml
